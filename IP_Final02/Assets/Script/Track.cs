0482028d936d9ffdf3a43363ba6bb117be733fce
luna11woolf <yw3264@nyu.edu>
2020-05-07 14:52:17 +0800

    Procedural Generation Track

---
+++ IP_Final02/Assets/Script/Track.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Track")]
+public class Track : ScriptableObject
+{
+    public GameObject[] Tracks;
+    public Vector2 trackSize = new Vector2(9f, 9f);
+
+    public enum Direction
+    {
+        North,
+        East,
+        South,
+        West
+    }
+    public Direction entrydir;
+    public Direction exitdir;
+
+
+}
---
+++ IP_Final02/Assets/Script/TrackGenerator.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackGenerator : MonoBehaviour
+{
+    public Track[] tracks;
+    public Track firstTrack;
+
+    private Track previousTrack;
+
+    public Vector3 spawnOrigin;
+    public Vector3 spawnPos;
+
+    public int spawnamount = 10;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            PickSpawnTrack();
+        }
+
+    }
+
+
+    void Start()
+    {
+        previousTrack = firstTrack;
+
+        for (int i = 0; i < spawnamount; i++)
+        {
+            PickSpawnTrack();
+        }
+        
+    }
+
+    void PickSpawnTrack()
+    {
+        Track curTrack = PickNextTrack();
+
+        GameObject objectfromTrack = curTrack.Tracks[0];
+        Instantiate(objectfromTrack, spawnPos + spawnOrigin, Quaternion.identity);
+        previousTrack = curTrack;
+
+    }
+
+    Track PickNextTrack()
+    {
+
+        List<Track> nextTrackList = new List<Track>();
+
+        Track nextTrack = null;
+
+        Track.Direction requiredDir = Track.Direction.South;
+
+        switch (previousTrack.exitdir)
+        {
+            case Track.Direction.North:
+                requiredDir = Track.Direction.South;
+                spawnPos = spawnPos + new Vector3(0, 0, previousTrack.trackSize.y);
+                break;
+
+            case Track.Direction.West:
+                requiredDir = Track.Direction.East;
+                spawnPos = spawnPos + new Vector3(- previousTrack.trackSize.x, 0, 0.5f);
+                break;
+
+            case Track.Direction.East:
+                //Debug.Log("Exit East");
+                requiredDir = Track.Direction.West;
+                spawnPos = spawnPos + new Vector3(previousTrack.trackSize.x, 0, 0.5f);
+                break;
+        }
+
+        for (int i = 0; i < tracks.Length; i ++)
+        {
+            if (tracks[i].entrydir == requiredDir)
+            {
+                nextTrackList.Add(tracks[i]);
+            }
+        }
+
+
+        nextTrack = nextTrackList[Random.Range(0, nextTrackList.Count)];
+        return nextTrack;
+
+    }
+
+
+
+}
