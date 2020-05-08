11d9245abc4386f9ad5b1d130a817805876d9f86
luna11woolf <yw3264@nyu.edu>
2020-05-09 05:30:02 +0800

    Comment for Track Generator script

--- IP_Final02/Assets/Script/TrackGenerator.cs
+++ IP_Final02/Assets/Script/TrackGenerator.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//This script is the trackGenerator that generate the track.
-//It will first generate 10 tiles and each times player move away from one tiles, it will add one to the front
+//This script is the trackGenerator that generate the tracks.
+//It will first generate 10 track. Each times player move away from one track, it will add one to the front
 // and remove the last one.
 public class TrackGenerator : MonoBehaviour
 {
@@ -14,20 +14,16 @@ public class TrackGenerator : MonoBehaviour
 
     private Track previousTrack;// previous Track that has been placed
 
-    public Vector3 spawnOrigin;
-    public Vector3 spawnPos;
+    //public Vector3 spawnOrigin;
+    public Vector3 spawnPos; // The position to spawn next tiles
 
-    public int spawnamount = 10;
+    public int spawnamount = 10; // The spawn amount at first.
 
-    public TravelObject travelObject;
+    public TravelObject travelObject; //the camera object that travel through the track
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            PickSpawnTrack();
-        }
-
+        // if there is more than 11 track, remove the last one
         if (trackInScene.Count > 11)
         {
             GameObject temp = trackInScene[0];
@@ -39,34 +35,37 @@ public class TrackGenerator : MonoBehaviour
 
     void Start()
     {
+        //set previousTrack as the first Track
         previousTrack = firstTrack;
 
+        //generate 10 tracks
         for (int i = 0; i < spawnamount; i++)
         {
             PickSpawnTrack();
         }
     }
 
+    //pick the track that match the exit & entry direction and put it int the position
     public void PickSpawnTrack()
     {
         Track curTrack = PickNextTrack();
-
-        GameObject objectfromTrack = curTrack.Tracks[0];
-        GameObject temp = Instantiate(objectfromTrack, spawnPos + spawnOrigin, Quaternion.identity);
-        trackInScene.Add(temp);
-        travelObject.desList.Add(spawnPos + spawnOrigin);
-        previousTrack = curTrack;
-
+        GameObject objectfromTrack = curTrack.Tracks[0]; //This format will be useful if there is more than one model of each track type
+        GameObject temp = Instantiate(objectfromTrack, spawnPos, Quaternion.identity);
+        trackInScene.Add(temp); //add this new track to the currenttrack list
+        travelObject.desList.Add(spawnPos); // Add the position to the travel destination list
+        previousTrack = curTrack; // set current track as previous track
     }
 
+    //pick the track that match the exit & entry direction
     Track PickNextTrack()
     {
+        //This format will be useful if there is more than one model of each track type
         List<Track> nextTrackList = new List<Track>();
-
         Track nextTrack = null;
-
+        // The enter  dirction that required for next track
         Track.Direction requiredDir = Track.Direction.South;
 
+        //check the exit direction of the perivous track and set the required enter direction for next track
         switch (previousTrack.exitdir)
         {
             case Track.Direction.North:
@@ -80,12 +79,13 @@ public class TrackGenerator : MonoBehaviour
                 break;
 
             case Track.Direction.East:
-                //Debug.Log("Exit East");
                 requiredDir = Track.Direction.West;
                 spawnPos = spawnPos + new Vector3(previousTrack.trackSize.x, 0, 0.5f);
                 break;
         }
 
+        //go through the track list to find the track that match the direction and add it to the nextTrackList
+        //This format will be useful if there is more than one model of each track type
         for (int i = 0; i < tracks.Length; i ++)
         {
             if (tracks[i].entrydir == requiredDir)
@@ -94,8 +94,11 @@ public class TrackGenerator : MonoBehaviour
             }
         }
 
+        // Go through nextTrackList and ramdomly pick one for next track
+        //This format will be useful if there is more than one model of each track type
+        //For this game, for now there will be only one possible track in the list
         nextTrack = nextTrackList[Random.Range(0, nextTrackList.Count)];
-        return nextTrack;
+        return nextTrack; // return the next Track
 
     }
 
